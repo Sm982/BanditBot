02569d5a5b8c054765c6e5dc2d779f91a340e266
Sm982 <iosdev41@gmail.com>
2025-03-05 21:22:55 +1030

    fix

--- cogs/communityNightCmd.py
+++ cogs/communityNightCmd.py
@@ -62,7 +62,7 @@ class commNightCMD(commands.Cog):
             print(f"Error in command: {str(error)}")
             
     @app_commands.command(name="communitynight", description="Schedule a community night")
-    @commands.has_any_role(1251353532037337200, 1251353440102121524)
+    @commands.has_any_role("Bandits Admins", "Bandits Shotcaller")
     async def scheduler(self, interaction: discord.Interaction, gamename: str, nextday: str, choosehour: int, chooseminutes: int, amorpm: str):
         errors = []
 
