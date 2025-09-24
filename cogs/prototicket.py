52285f6e25b9269e6816c18a42b894827ae6c500
Sm982 <iosdev41@gmail.com>
2025-09-24 20:09:11 +0930

    final commit for the night

--- cogs/prototicket.py
+++ cogs/prototicket.py
@@ -61,7 +61,8 @@ class ProtoTicket(commands.Cog):
         await channel.send(embed=embed, view=view)
 
         thread = await channel.create_thread(name=f"Staff-#{ticketNumber}", type=discord.ChannelType.public_thread, reason=f"Staff discussion for ticket #{ticketNumber}")
-        staff_role = discord.utils.get(guild.roles, id=1044403662996373609)
+        staff_role_id= 1044403662996373609
+        staff_role = discord.utils.get(guild.roles, id=staff_role_id)
         if staff_role:
         
             for member in staff_role.members:
@@ -70,7 +71,7 @@ class ProtoTicket(commands.Cog):
                 except discord.HTTPException:
                     pass  # Skip if user can't be added
         
-        await thread.send(f"<@{staff_role}> 🔒 **Staff Only Discussion**\nThis is a private discussion thread for Ticket #{ticketNumber}")
+        await thread.send(f"<@{staff_role_id}> 🔒 **Staff Only Discussion**\nThis is a private discussion thread for Ticket #{ticketNumber}")
         await interaction.response.send_message("Ticket created", ephemeral=True)
 
 
